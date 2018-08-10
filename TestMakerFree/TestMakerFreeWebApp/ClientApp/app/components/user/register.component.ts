41e4a0c26de7d08b588cdb3fbb6ce4d2812ee286
Duy <duyhthai@gmail.com>
2018-08-10 19:43:29 +0700

    registration

---
+++ TestMakerFree/TestMakerFreeWebApp/ClientApp/app/components/user/register.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, Inject } from "@angular/core";
+import { FormGroup, FormControl, FormBuilder, Validators  } from "@angular/forms";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+    selector: "register",
+    templateUrl: "./register.component.html",
+    styleUrls: ['./register.component.css']
+})
+
+export class RegisterComponent {
+    title: string;
+    form: FormGroup;
+
+    constructor(
+        private router: Router,
+        private fb: FormBuilder,
+        private http: HttpClient,
+        @Inject('BASE_URL') private baseUrl: string) {
+
+        this.title = "New User Registration";
+    }
+}
