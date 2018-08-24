3a2d31ec30f2000a9827a2aaaeaa4aacb798fcc3
Duy <duyhthai@gmail.com>
2018-08-24 19:15:19 +0700

    open exchange api

---
+++ CurrencyExchange/CurrencyExchange.Core/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RestSharp;
+
+namespace CurrencyExchange.Core
+{
+    public static class Utilities
+    {
+        public static IRestResponse ExecuteApi(string apiUrl, Method method = Method.GET)
+        {
+            var client = new RestClient(apiUrl);
+            var request = new RestRequest(method);
+            return client.Execute(request);
+        }
+    }
+}
--- CurrencyExchange/CurrencyExchange.Predictor/Program.cs
+++ CurrencyExchange/CurrencyExchange.Predictor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using CurrencyExchange.Core;
 using Microsoft.Extensions.Configuration;
 
 namespace CurrencyExchange.Predictor
@@ -10,32 +11,31 @@ namespace CurrencyExchange.Predictor
 
         public static void Main()
         {
-            var xValues = new double[]
-                              {
-                                  1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-                                  2005, 2006, 2007, 2008, 2009
-                              };
-            var yValues = new double[]
-                              {
-                                  8669269, 8595500, 8484900, 8459800, 8427400, 8384700, 8340900, 8283200, 8230400, 8190900,
-                                  8149468, 7932984, 7845841, 7801273, 7761049, 7720000, 7679290, 7640238, 7606551,
-                                  7563710
-                              };
-
-            double rSquared, intercept, slope;
-            LinearRegression(xValues, yValues, out rSquared, out intercept, out slope);
-
-            Console.WriteLine($"R-squared = {rSquared}");
-            Console.WriteLine($"Intercept = {intercept}");
-            Console.WriteLine($"Slope = {slope}");
-
-            var predictedValue = (slope * 2017) + intercept;
-            Console.WriteLine($"Prediction for 2017: {predictedValue}");
+            ////var xValues = new double[]
+            ////                  {
+            ////                      1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004,
+            ////                      2005, 2006, 2007, 2008, 2009
+            ////                  };
+            ////var yValues = new double[]
+            ////                  {
+            ////                      8669269, 8595500, 8484900, 8459800, 8427400, 8384700, 8340900, 8283200, 8230400, 8190900,
+            ////                      8149468, 7932984, 7845841, 7801273, 7761049, 7720000, 7679290, 7640238, 7606551,
+            ////                      7563710
+            ////                  };
+            ////double rSquared, intercept, slope;
+            ////LinearRegression(xValues, yValues, out rSquared, out intercept, out slope);
+            ////Console.WriteLine($"R-squared = {rSquared}");
+            ////Console.WriteLine($"Intercept = {intercept}");
+            ////Console.WriteLine($"Slope = {slope}");
+            ////var predictedValue = (slope * 2017) + intercept;
+            ////Console.WriteLine($"Prediction for 2017: {predictedValue}");
 
             BuildConfiguration();
 
             Console.WriteLine("Hello World!");
             Console.WriteLine($"OpenExchange app id: {Configuration["OpenExchange:AppID"]}");
+            var oxrHelper = new OpenExchangeRatesHelper(Configuration["OpenExchange:AppID"]);
+            oxrHelper.GetHistoricalData("2001-02-16");
         }
 
         private static void BuildConfiguration()
