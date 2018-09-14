055c5048d68de6a5ab8a667bb0f4c552df33905c
Duy <duyhthai@gmail.com>
2018-09-14 18:14:50 +0700

    data context

--- ECommerce/Startup.cs
+++ ECommerce/Startup.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ECommerce.Data;
+using ECommerce.Data.Entities;
 using ECommerce.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -18,12 +22,18 @@ namespace ECommerce
         {
             Configuration = configuration;
         }
-
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EcommerceContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<EcommerceContext>()
+                .AddDefaultTokenProviders();
+
             services.AddMvc();
 
             services.Configure<RazorViewEngineOptions>(options =>
