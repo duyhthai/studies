4caff648a078eb89a1892e5d8d944493dea0b10d
Duy <duyhthai@gmail.com>
2018-09-14 12:10:20 +0700

    rename project

--- VueJS/Program.cs
+++ VueJS/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace VueJS
+namespace ECommerce
 {
     public class Program
     {
