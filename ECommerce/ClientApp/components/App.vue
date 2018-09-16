4c1543b7510507d68f2bd3867471bb54209b94a9
Duy <duyhthai@gmail.com>
2018-09-16 23:30:21 +0700

    product list and details

--- ECommerce/ClientApp/components/App.vue
+++ ECommerce/ClientApp/components/App.vue
@@ -1,37 +1,17 @@
 <template>
-    <div>
-        <h1>Welcome to Hands on Vue.js with ASP.NET Core!</h1>
-        <p>
-            The time is: {{ time }}
-        </p>
-        <p>
-            The current users of our system are:
-            <ul>
-                <li v-for="user in users">
-                    {{ user.fullName }} - {{ user.userName }}
-                </li>
-            </ul>
-        </p>
-    </div>
+  <div>
+    <product-list />
+  </div>
 </template>
 
+
 <script>
-export default {
-    name: 'app',
-    data () {
-        return {
-            time: new Date().toString(),
-            users: []
+    import ProductList from "./products/List.vue";
+
+    export default {
+        name: "app",
+        components: {
+            ProductList
         }
-    },
-    mounted() {
-        fetch('/api/users')
-            .then(response => {
-                return response.json()
-            })
-            .then(data => {
-                this.users = data
-            })
-    }
-}
+    };
 </script>
