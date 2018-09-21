203da4082c637187dc4ac95140463efbbe6d12bc
Duy <duyhthai@gmail.com>
2018-09-21 12:00:17 +0700

    navbar

--- ECommerce/ClientApp/components/App.vue
+++ ECommerce/ClientApp/components/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <router-view />
+<div class="app">
+    <navbar></navbar>
+
+    <router-view />
+</div>
 </template>
 
 
 <script>
+    import Navbar from "./Navbar.vue";
+
     export default {
-        name: "app"        
+        name: "app",
+        components: {
+          Navbar
+        }     
     };
 </script>
+
+
+<style lang="scss">
+html,
+body {
+  height: 100vh;
+}
+div.app,
+div.page {
+  height: 100% !important;
+}
+</style>
---
+++ ECommerce/ClientApp/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-brand to="/">PhoneShop</b-navbar-brand>
+        <b-collapse is-nav id="nav_collapse">
+          <b-navbar-nav>
+            <b-nav-item to="/products">Products</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: "navbar"
+};
+</script>
