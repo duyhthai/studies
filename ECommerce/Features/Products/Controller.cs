23a825ff1763a5ef709581bd7802c8393b12c897
Duy <duyhthai@gmail.com>
2018-09-26 17:58:25 +0700

    filtering on the server

--- ECommerce/Features/Products/Controller.cs
+++ ECommerce/Features/Products/Controller.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using ECommerce.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -16,9 +19,35 @@ namespace ECommerce.Features.Products
     }
 
     [HttpGet]
-    public async Task<IActionResult> Find()
+    public async Task<IActionResult> Find(string brands, int? minPrice, int? maxPrice, int? minScreen, int? maxScreen, string capacity, string colours, string os, string features)
     {
-        var products = await _db.Products.ToListAsync();
+        var Brands = string.IsNullOrWhiteSpace(brands) ? new List<string>() : brands.Split('|').ToList();
+        var Capacity = string.IsNullOrWhiteSpace(capacity) ? new List<int>() : capacity.Split('|').Select(int.Parse).ToList();
+        var Colours = string.IsNullOrWhiteSpace(colours) ? new List<string>() : colours.Split('|').ToList();
+        var OS = string.IsNullOrWhiteSpace(os) ? new List<string>() : os.Split('|').ToList();
+        var Features = string.IsNullOrWhiteSpace(features) ? new List<string>() : features.Split('|').ToList();
+
+        var products = await _db.Products
+            .Where(x => !Brands.Any() || Brands.Contains(x.Brand.Name))
+            .Where(x => !minPrice.HasValue || x.ProductVariants.Any(v => v.Price >= minPrice.Value))
+            .Where(x => !maxPrice.HasValue || x.ProductVariants.Any(v => v.Price <= maxPrice.Value))
+            .Where(x => !minScreen.HasValue || x.ScreenSize >= Convert.ToDecimal(minScreen.Value))
+            .Where(x => !maxScreen.HasValue || x.ScreenSize <= Convert.ToDecimal(maxScreen.Value))
+            .Where(x => !Capacity.Any() || x.ProductVariants.Any(v => Capacity.Contains(v.Storage.Capacity)))
+            .Where(x => !Colours.Any() || x.ProductVariants.Any(v => Colours.Contains(v.Colour.Name)))
+            .Where(x => !OS.Any() || OS.Contains(x.OS.Name))
+            .Where(x => !Features.Any() || Features.All(f => x.ProductFeatures.Any(pf => pf.Feature.Name == f)))
+            .Select(x => new ProductListViewModel
+            {
+              Id = x.Id,
+              Slug = x.Slug,
+              Name = x.Name,
+              ShortDescription = x.ShortDescription,
+              Thumbnail = x.Thumbnail,
+              Price = x.ProductVariants.OrderBy(v => v.Price).First().Price
+            })
+            .ToListAsync();
+
         return Ok(products);
     }
 
