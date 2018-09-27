5fae8919767b627b8857a6a67f9520df74b480f0
Duy <duyhthai@gmail.com>
2018-09-27 16:34:38 +0700

    watcher & debouncing

--- ECommerce/ClientApp/components/catalogue/SearchBar.vue
+++ ECommerce/ClientApp/components/catalogue/SearchBar.vue
@@ -4,7 +4,7 @@
       :value="query"
       type="text"
       placeholder="Search..."
-      @change="update"
+      @input="update"
       @keyup.enter.native="search">
     </b-form-input>
   </div>
@@ -12,6 +12,8 @@
 
 
 <script>
+import _ from "lodash";
+
 export default {
   name: "search-bar",
   data() {
@@ -28,16 +30,21 @@ export default {
     update(newVal) {
       this.value = newVal;
     },
-    search() {
-      let query = Object.assign({}, this.$route.query);
+    search: _.debounce(function() {
+        let query = Object.assign({}, this.$route.query);
 
-      if (this.value.trim()) {
-        query.q = this.value;
-      } else {
-        delete query.q;
-      }
+        if (this.value.trim()) {
+            query.q = this.value;
+        } else {
+            delete query.q;
+        }
 
-      this.$router.push({ query: query });
+        this.$router.push({ query: query });
+    }, 500)
+  },
+  watch: {
+    value(newValue) {
+        this.search();
     }
   }
 };
