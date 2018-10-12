6eeefe34e0f8c4166b2c86ab62fe661569e74646
Duy <duyhthai@gmail.com>
2018-10-12 18:07:58 +0700

    JWT auth

--- ECommerce/Startup.cs
+++ ECommerce/Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ECommerce.Data;
 using ECommerce.Data.Entities;
 using ECommerce.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -13,6 +15,7 @@ using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ECommerce
 {
@@ -22,7 +25,7 @@ namespace ECommerce
         {
             Configuration = configuration;
         }
-        
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -36,6 +39,33 @@ namespace ECommerce
             services.AddIdentity<AppUser, AppRole>()
                 .AddEntityFrameworkStores<EcommerceContext>()
                 .AddDefaultTokenProviders();
+
+            // JWT authentication
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.ClaimsIssuer = Configuration["Authentication:JwtIssuer"];
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration["Authentication:JwtIssuer"],
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["Authentication:JwtAudience"],
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Authentication:JwtKey"])),
+                        RequireExpirationTime = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
 
             // MVC
             services.AddMvc();
@@ -67,6 +97,8 @@ namespace ECommerce
             }
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
