f3417fdaeb3650820bedca3679d8775f14e9b49c
Duy <duyhthai@gmail.com>
2018-10-25 14:29:16 +0700

    add Stripe, Vee-Validate

--- ECommerce/ClientApp/boot.js
+++ ECommerce/ClientApp/boot.js
@@ -5,10 +5,12 @@ import BootstrapVue from "bootstrap-vue";
 import NProgress from "nprogress";
 import VueToastr from "@deveodk/vue-toastr";
 import "@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css";
+import VeeValidate from "vee-validate";
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VueToastr, { defaultPosition: "toast-top-right" });
+Vue.use(VeeValidate);
 
 // filters
 import { currency } from "./filters";
