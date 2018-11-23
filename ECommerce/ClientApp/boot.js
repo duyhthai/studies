9cce133de4e120d71758d2fde4ba1384880dd061
Duy <duyhthai@gmail.com>
2018-11-23 18:46:56 +0700

    refresh token

--- ECommerce/ClientApp/boot.js
+++ ECommerce/ClientApp/boot.js
@@ -5,7 +5,10 @@ import BootstrapVue from "bootstrap-vue";
 import VueToastr from "@deveodk/vue-toastr";
 import "@deveodk/vue-toastr/dist/@deveodk/vue-toastr.css";
 import VeeValidate from "vee-validate";
+
+// helpers
 import "./helpers/validation";
+import "./helpers/interceptors";
 
 Vue.use(BootstrapVue);
 Vue.use(VueToastr, { defaultPosition: "toast-top-right" });
